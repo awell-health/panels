bf224d3dcbcc53e09f32c0159486fbd713a65fec
Pawel Skroban <pskrobek@gmail.com>
2025-07-07 09:31:19 +0200

    [PLA-461] Close task modal on click outside & esc key

--- apps/app/src/app/panel/[panel]/components/ModalDetails/ModalDetails.tsx
+++ apps/app/src/app/panel/[panel]/components/ModalDetails/ModalDetails.tsx
@@ -3,6 +3,7 @@ import PatientDetails from './PatientDetails/PatientDetails'
 import TaskDetails from './TaskDetails/TaskDetails'
 import { User, X } from 'lucide-react'
 import TaskStatusBadge from './TaskDetails/TaskStatusBadge'
+import { useEffect, useRef } from 'react'
 
 interface ModalDetailsProps {
   row: WorklistPatient | WorklistTask
@@ -14,9 +15,38 @@ const ModalDetails = ({ row, onClose }: ModalDetailsProps) => {
   const patientName = patient?.name || row.name || ''
   const dateOfBirth = patient?.birthDate || row.birthDate || ''
 
+  const modalRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose()
+      }
+    }
+
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        onClose()
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+    document.addEventListener('keydown', handleEsc)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+      document.removeEventListener('keydown', handleEsc)
+    }
+  }, [onClose])
+
   return (
     <dialog className="modal modal-open">
-      <div className="modal-box max-w-[95vw] min-h-[70vh] max-h-[80vh] p-0 flex flex-col">
+      <div
+        className="modal-box max-w-[95vw] min-h-[70vh] max-h-[80vh] p-0 flex flex-col"
+        ref={modalRef}
+      >
         <div className="h-12 border-b border-gray-200 bg-gray-50 flex items-center justify-between flex-shrink-0">
           <div className="flex items-center gap-2 text-sm text-gray-700 pl-4">
             <User className="h-5 w-5" />
