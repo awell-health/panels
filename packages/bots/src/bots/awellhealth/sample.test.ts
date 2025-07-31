c5b8d9581b5f2fc11eece6b2f4f8f7d2b8c6d6fc
Etienne Bömcke <etienne@awellhealth.com>
2025-07-31 11:55:23 +0200

    chore: add sample test in the bots package

---
+++ packages/bots/src/bots/awellhealth/sample.test.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest'
+
+describe('Sample bot test', () => {
+  it('This is where you can write tests for the functions used in bots', () => {
+    expect(true).toBe(true)
+  })
+})
